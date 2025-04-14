a8395112aa35609681e4360b3d4ebdab36ee0307
jmensah365 <jmensah365@gmail.com>
2025-04-14 16:18:24 -0400

    removing react and vite svg imports

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import SDEPage from './components/SDEPage/SDEPage.jsx'
 import './App.css'
 import HomePage from './components/HomePage'
