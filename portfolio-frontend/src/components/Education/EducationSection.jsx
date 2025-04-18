6bc27ae4853cbc09f996468cbd114d9897282057
jmensah365 <jmensah365@gmail.com>
2025-04-17 23:32:42 -0400

    swapping the cards so education bio

--- portfolio-frontend/src/components/Education/EducationSection.jsx
+++ portfolio-frontend/src/components/Education/EducationSection.jsx
@@ -9,10 +9,11 @@ function EducationSection() {
             <h2 className="text-5xl font-bold mb-10 text-shadow-lg/80">About Me</h2>
             {/* Education Section */}
             <div className="flex flex-col space-y-10 md:flex-row md:space-x-10 md:space-y-0 justify-center items-stretch">
-                {/* COMPS Senior Project Card */}
-                <COMPSCard/>
                 {/* Education Card */}
                 <EducationCard/>
+                
+                {/* COMPS Senior Project Card */}
+                <COMPSCard/>
             </div>
             <AboutMe/>
         </div>
