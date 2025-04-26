ed780946aa852ff94f774ab2d34f8ead497969e7
jmensah365 <jmensah365@gmail.com>
2025-04-26 00:18:56 -0400

    adding more padding above resume section

--- portfolio-frontend/src/components/Resume/MyResume.jsx
+++ portfolio-frontend/src/components/Resume/MyResume.jsx
@@ -1,7 +1,7 @@
 
 function MyResume() {
     return (
-        <div className='flex flex-col justify-center items-center h-screen pt-20 gap-5'>
+        <div className='flex flex-col justify-center items-center h-screen pt-48 gap-5'>
                 <h2 className='text-white text-5xl font-roboto font-bold' id='My Resume'>My Resume</h2>
                 <embed src='/JM_04_25.pdf' type='application/pdf' className='w-full max-w-screen-md h-full max-h-screen-md border rounded=lg'/>
         </div>
