caa21d78a1f898e2451ccd42e8294a9ec4f717bd
jmensah365 <jmensah365@gmail.com>
2025-07-15 13:54:28 -0400

    updating resume

--- portfolio-frontend/src/components/Resume/MyResume.jsx
+++ portfolio-frontend/src/components/Resume/MyResume.jsx
@@ -1,9 +1,9 @@
 
 function MyResume() {
     return (
-        <div className='flex flex-col justify-center items-center h-screen pt-48 gap-5'>
+        <div className='flex flex-col justify-center items-center h-screen pt-48 gap-5 scroll-smooth'>
                 <h2 className='text-white text-5xl font-roboto font-bold' id='My Resume'>My Resume</h2>
-                <embed src='/JM_04_25.pdf' type='application/pdf' className='w-full max-w-screen-md h-full max-h-screen-md border rounded=lg'/>
+                <embed src='/Jeremiah_Mensah_Resume.pdf' type='application/pdf' className='w-full max-w-screen-md h-full max-h-screen-md border rounded=lg'/>
         </div>
     )
 }
